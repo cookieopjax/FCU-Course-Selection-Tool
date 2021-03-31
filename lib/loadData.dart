f56737cb4f0ef177704a06183602ab3e57c9c95d
cookieopjax <vigor01695@gmail.com>
2021-03-31 15:42:10 +0800

    data testing

--- lib/loadData.dart
+++ lib/loadData.dart
@@ -1,21 +1,37 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-class LoadData{
-  String lang, week, period;
-  int year, sms;
-  bool codeEnable, weekPeriodEnable, courseEnable, teacherEnable,
-      useEnglishEnable, useLanguageEnable, specificSubjectEnable, courseDescriptionEnable;
-  String codeValue, courseValue, teacherValue, useLanguageValue, specificSubjectValue, courseDescriptionValue;
+class LoadData {
+  int year, //學年度 : 110、109、108
+      sms, //學期 : 1(上學期)、2(下學期)、3(暑修上)、4(暑修下)
+      week, //週 : 1、2....、7
+      period, //節 : "*"代表全部，0、1、2、....、7
+      specificSubjectValue; //1(通識)、2(體育)、3(國文)
+
+  bool codeEnable, //是否指定選課代號
+      weekPeriodEnable, //是否指定週節
+      courseEnable, //是否指定科目名稱
+      teacherEnable, //使否指定開課教師姓名
+      useEnglishEnable, //這是啥鬼
+      useLanguageEnable, //是否指定授課語言
+      specificSubjectEnable, //是否指定特定課程
+      courseDescriptionEnable; //使否指定課程描述
+
+  String lang, //資料的語言，包含"cht"及"en"
+      codeValue, //選課代號
+      courseValue, //科目名稱
+      teacherValue, //開課教師
+      useLanguageValue, //授課語言
+      courseDescriptionValue; //課程描述
 
   LoadData({
     this.lang = "cht",
-    this.week = "1", //temp
-    this.period = "3", //temp
+    this.week = 1, //temp
+    this.period = 1, //temp
     this.year = 109,
     this.sms = 2,
     this.codeEnable = false,
-    this.weekPeriodEnable = false,
+    this.weekPeriodEnable = true, //temp
     this.courseEnable = false,
     this.teacherEnable = false,
     this.useEnglishEnable = false,
@@ -26,33 +42,41 @@ class LoadData{
     this.courseValue = "",
     this.teacherValue = "",
     this.useLanguageValue = "01", //temp
-    this.specificSubjectValue = "1", //temp
+    this.specificSubjectValue = 1, //temp
     this.courseDescriptionValue = "",
   });
 
   Future<String> getData() async {
     var url = Uri.parse(
-      'https://coursesearch02.fcu.edu.tw/Service/Search.asmx/GetType2Result');
+        'https://coursesearch02.fcu.edu.tw/Service/Search.asmx/GetType2Result');
     var payload = {
       "baseOptions": {"lang": lang, "year": year, "sms": sms},
       "typeOptions": {
         "code": {"enabled": codeEnable, "value": codeValue},
-        "weekPeriod": {"enabled": weekPeriodEnable, "week": week, "period": period},
+        "weekPeriod": {
+          "enabled": weekPeriodEnable,
+          "week": week,
+          "period": period
+        },
         "course": {"enabled": courseEnable, "value": courseValue},
         "teacher": {"enabled": teacherEnable, "value": teacherValue},
         "useEnglish": {"enabled": useEnglishEnable},
-        "useLanguage": {"enabled": useLanguageEnable, "value": useLanguageValue},
-        "specificSubject": {"enabled": specificSubjectEnable, "value": specificSubjectValue},
+        "useLanguage": {
+          "enabled": useLanguageEnable,
+          "value": useLanguageValue
+        },
+        "specificSubject": {
+          "enabled": specificSubjectEnable,
+          "value": specificSubjectValue
+        },
         "courseDescription": {"enabled": courseEnable, "value": courseValue}
       }
     };
     var header = {"Content-Type": "application/json"};
     var body = json.encode(payload);
     var response = await http.post(url, headers: header, body: body);
-    var data = json.decode(response.body);
-    var data2 = json.decode(data['d']);
-    print(data2.runtimeType);
-    print(data2['total']);
-    return response.body;
+    var data = json.decode(json.decode(response.body)['d']);
+    print(data.toString());
+    return data['items'].toString();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -9,32 +9,35 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '逢甲課程檢索',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: TestData(),
-    );
+        title: '逢甲課程檢索',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('逢甲檢索系統'),
+          ),
+          body: TestData(),
+        ));
   }
 }
 
 class TestData extends StatelessWidget {
-  var data = LoadData();
+  final data = LoadData(); //建立資料物件
   @override
   Widget build(BuildContext context) {
+    data.week = 3;
     return Center(
       child: FutureBuilder<String>(
-        future: data.getData(),
+        future: data.getData(), //依據物件的成員變數取資料
         builder: (BuildContext context, AsyncSnapshot snapshot) {
           if (snapshot.connectionState == ConnectionState.done) {
             if (snapshot.hasError) {
               return Text("Error: ${snapshot.error}");
             } else {
-              // 请求成功，显示数据
-              return Text("Contents: ${snapshot.data}");
+              return Text("資料 : ${snapshot.data}");
             }
           } else {
-            // 请求未结束，显示loading
             return CircularProgressIndicator();
           }
         },
@@ -42,6 +45,3 @@ class TestData extends StatelessWidget {
     );
   }
 }
-
-
-
